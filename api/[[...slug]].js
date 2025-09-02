41c329d0bce329c9fecefd718a6c74acefe2b609
AKL561 <a.lakel@yahoo.com>
2025-09-02 09:05:04 +0200

    fix(vercel): export app + /health + proxy /api/* vers Express

--- api/[[...slug]].js
+++ api/[[...slug]].js
@@ -1,7 +1,1 @@
-const app = require('../app');
-
-// Redirige /api/* vers les routes Express, en retirant le /api
-module.exports = (req, res) => {
-  req.url = req.url.replace(/^\/api/, '');
-  return app(req, res);
-};
+const app=require('../app');module.exports=(req,res)=>{req.url=req.url.replace(/^\/api/,'');return app(req,res);};
