b3272aa8d880ca0876e2dcfb4ca92c05b3a53600
AKL561 <a.lakel@yahoo.com>
2025-09-02 09:19:41 +0200

    fix(api): export Express app + proxy /api/* + /health

--- api/[[...slug]].js
+++ api/[[...slug]].js
@@ -1,1 +1,5 @@
-const app=require('../app');module.exports=(req,res)=>{req.url=req.url.replace(/^\/api/,'');return app(req,res);};
+const app = require('../app');
+module.exports = (req, res) => {
+  req.url = req.url.replace(/^\/api/, ''); // /api/todos -> /todos
+  return app(req, res);
+};
